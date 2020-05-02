e7dbf7f096088b9e812264c3af6bb955e466607e
Yuxin T <tangtang5yo@gmail.com>
2020-05-02 01:42:47 +0100

    update Genetic.js

--- Genetic.js
+++ Genetic.js
@@ -22,16 +22,14 @@ lessons.push(new lesson(8, "sport", "esabelle", ["g2"], 1, false));
 lessons.push(new lesson(9, "history", "francois", ["g3"], 1, false));
 lessons.push(new lesson(10, "french", "mary", ["g3"], 1, false));
 
-console.log(lessons);
+//document.getElementById("lesson").innerHTML = JSON.stringify(lessons);
 
 
 
 
 function initialise(populationSize, LessonSize, timespaceSize) {
-    // max = timespaceSize;
-    // let integer = Math.floor(Math.random() * Math.floor(max)); // return random integer from 0 to max
     population = [];
-    for (i = 0; i < populationSize; i++) {
+    for (ips = 0; ips < populationSize; ips++) {
         chromosome = [];
         while (chromosome.length < LessonSize) {
             var r = Math.floor(Math.random() * Math.floor(timespaceSize));
@@ -43,6 +41,7 @@ function initialise(populationSize, LessonSize, timespaceSize) {
     return population;
 }
 
+//fitness for one chromosome
 function fitness(chromosome) {
     score = 0;
     for (i = 0; i < chromosome.length; i++) {
@@ -70,7 +69,7 @@ function fitness(chromosome) {
                 if (bool) { score++; }
 
             }
-            else { score+=2;}
+            else { score += 2; }
 
             //check if students are free at time
         }
@@ -94,34 +93,111 @@ function fitness(chromosome) {
                 }
                 if (bool) { score++; }
             }
-            else { score += 2;}
+            else { score += 2; }
         }
         //check if 2 hours course can be delivered
-        if ((chromosome[i] % 7 != 2 && chromosome[i] % 7 != 6 && !chromosome.includes(chromosome[i] + 1)) || lessons[i].duration === 1){
-            score++; 
+        if ((chromosome[i] % 7 != 2 && chromosome[i] % 7 != 6 && !chromosome.includes(chromosome[i] + 1)) || lessons[i].duration === 1) {
+            score++;
         }
     }
-    return score/(5*chromosome.length);
+    return score / (5 * chromosome.length);
 }
 
-function select() {
+function selectParents(population) {
+    sum = 0;
+    population.forEach(e => { sum += e.fitness; });
 
+    p1 = Math.floor(Math.random() * Math.floor(sum));
+    let i = 0;
+    while (p1 < sum) {
+        p1 += population[i].fitness;
+        i++;
+    }
+    i--;
+    p2 = Math.floor(Math.random() * Math.floor(sum));
+    let j = 0;
+    while (p2 < sum) {
+        p2 += population[j].fitness;
+        j++;
+    }
+    j--;
+    return { mother: population[i], father: population[j] };
 }
 
-function crossover() {
-
+// One point crossover
+//l is length of chromosome,number of genes
+//n is number of timespace slot
+function crossover(chromosome1, chromosome2, l, n) {
+    p = Math.floor(Math.random() * Math.floor(l-1));
+    child1 = chromosome1.slice(0,p+1) + "," + chromosome2.slice(p+1, l);
+    child2 = chromosome2.slice(0,p+1) + "," + chromosome1.slice(p+1, l);
+    child1 = child1.split(",").map(x => +x);
+    child2 = child2.split(",").map(x => +x);
+    let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
+    duplicates1 = findDuplicates(child1);
+    while (duplicates1.length > 0) {
+        g = Math.floor(Math.random() * Math.floor(n));
+        child1[child1.indexOf(duplicates1[0])] = g;
+        duplicates1 = findDuplicates(child1);
+    }
+    duplicates2 = findDuplicates(child2);
+    while (duplicates2.length > 0) {
+        g = Math.floor(Math.random() * Math.floor(n));
+        child2[child2.indexOf(duplicates2[0])] = g;
+        duplicates2 = findDuplicates(child2);
+    }
+    return { child1: child1, child2: child2 };
 }
 
-function mutation() {
-
+//swap mutation of two random genes
+function mutation(chromosome, l) {
+    p1 = Math.floor(Math.random() * Math.floor(l));
+    p2 = Math.floor(Math.random() * Math.floor(l));
+    [chromosome[p1], chromosome[p2]] = [chromosome[p2], chromosome[p1]];
+    //return chromosome;
 }
 
 
-
-function evolve() {
-
+//l = chromosome length
+//n = timespace slot number
+//s = population size
+function evolve(population, l, n, s, pc, pm) {
+    population.sort(function (a, b) {
+        return b.fitness - a.fitness;
+    });
+    parents = selectParents(population);
+    if (Math.random() <= pc) {
+        children = crossover(parents.mother.chromosome, parents.father.chromosome, l, n);
+    }
+    else {
+        children = {
+            child1: parents.mother.chromosome,
+            child2: parents.father.chromosome
+        }
+    }
+    if(Math.random() <= pm){
+        mutation(children.child1, l);
+    }
+    if(Math.random() <= pm){
+        mutation(children.child2, l);
+    }
+    population[s-1] = {chromosome:children.child1, fitness: fitness(children.child1)};
+    population[s-2] = {chromosome:children.child2, fitness: fitness(children.child2)};
 }
 
-var initialPopulation = initialise(15, 10, 70);
-console.log(initialPopulation[0]);
-console.log("final score is ",fitness(initialPopulation[0]));
+l = 10; n = 70; s = 15;
+var initialPopulation = initialise(15, 10, 70);  //l = 10, n = 70
+population = [];
+initialPopulation.forEach(element => {
+    population.push({ chromosome: element, fitness: fitness(element) });
+});
+evolve(population, l, n, s, 0.8, 0.2 );
+for(generation = 0; generation < 200; generation++){
+    console.log("generation: ",generation, "fit: " ,population[0].fitness);
+    if(population[0].fitness<1)
+        evolve(population, l, n, s, 0.8, 0.2 );
+    else break;
+}
+console.log(population);
+
+//document.getElementById("population").innerHTML = JSON.stringify(initialPopulation);
